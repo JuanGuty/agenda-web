692317dcbfd01cb05b7c8e0987e5b9f5f1c46234
JuanGuty <41837566+JuanGuty@users.noreply.github.com>
2018-11-06 02:47:34 -0600

    Update my-table.component.ts

--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -1,79 +1,42 @@
-import { DataSource } from '@angular/cdk/table';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { MyTableDataSource } from './my-table-datasource';
 import { PersonService } from '../services/person.service';
+import { DataSource } from '@angular/cdk/table';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-my-table',
   templateUrl: './my-table.component.html',
-  styleUrls: ['./my-table.component.scss']
+  styleUrls: ['./my-table.component.scss'],
 })
 export class MyTableComponent implements OnInit {
-  persons = new BehaviorSubject<any[]>([]);
-  dataSource = new PersonDataSource(this.persons);
+  dataSource = new PersonDataSource(this.personService);
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = [
-    'name',
-    'lastname',
-    'nick',
-    'actions'
-  ];
+  displayedColumns = ['name', 'lastName', 'nick'];
 
-  constructor(
-    private personService: PersonService,
-    private router: Router
-  ) {
-    // Load local files
-    this.personService.getConfig().subscribe(config => {
+  constructor(private personService: PersonService) {
+    this.personService.getConfig().subscribe( config => {
       console.log(config);
     });
 
-    this.personService
-      .getPersons()
-      .subscribe((persons: any[]) => {
-        this.persons.next(persons);
-      });
+    // this.personService.getPersons().subscribe( persons => {
+    //  console.log(persons);
+    // });
   }
 
-  ngOnInit() {}
-
-  update(person) {
-    localStorage.setItem('person', JSON.stringify(person));
-    this.router.navigate(['/nuevo']);
-  }
-
-  delete(person) {
-    this.personService.deletePerson(person.id).subscribe(
-      response => {
-        console.log('OK: ', response);
-
-        // Remove deleted person from the current data collection;
-        const tmp = this.persons.value.filter(
-          p => p.id !== person.id
-        );
-
-        // Update data source stream
-        this.persons.next(tmp);
-      },
-      error => {
-        console.log('ERROR: ', error);
-      }
-    );
+  ngOnInit() {
   }
 }
 
 export class PersonDataSource extends DataSource<any> {
-  persons: BehaviorSubject<any>;
-
-  /** Stream of data that is provided to the table. */
-  constructor(persons: BehaviorSubject<any>) {
+  constructor(private personService: PersonService) {
     super();
-    this.persons = persons;
   }
 
   connect(): Observable<any> {
-    return this.persons;
+    return this.personService.getPersons();
   }
 
   disconnect() {}
