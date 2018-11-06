cfa4293214e8908178fc8771a625fb28f01593c1
Juan Gutierrez <juan_guty@hotmail.com>
2018-11-06 04:14:30 -0600

    Upload FINAL_2

--- src/app/new-form/new-form.component.ts
+++ src/app/new-form/new-form.component.ts
@@ -1,31 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { PersonService } from '../services/person.service';
 
 @Component({
   selector: 'app-new-form',
   templateUrl: './new-form.component.html',
-  styleUrls: ['./new-form.component.scss']
+  styleUrls: ['./new-form.component.scss'],
 })
-export class NewFormComponent implements OnInit {
-  isNewPerson = true;
-
+export class NewFormComponent {
   personForm = this.fb.group({
-    id: [null],
     name: [null, Validators.required],
-    lastname: [null, Validators.required],
-    email: [null, [Validators.email, Validators.required]],
-    phone: [null, Validators.required],
-    nick: [null],
-    avatar: [null]
+    lastName: [null, Validators.required],
+    phone: [null, Validators.required]
   });
 
-  constructor(
-    private fb: FormBuilder,
-    private personService: PersonService,
-    private router: Router
-  ) {}
+  constructor(private fb: FormBuilder, private personService: PersonService) {}
 
   onSubmit() {
     console.log(this.personForm.value);
